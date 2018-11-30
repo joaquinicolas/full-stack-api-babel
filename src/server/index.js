311615d6091cf9c7dfa8a121d6d42f531e4fa6ed
Joaquin Nicolas <joako.n96@gmail.com>
2018-11-30 16:26:51 -0300

    support cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import {
   userRouter,
 } from './user';
 
-
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use('/', userRouter);
 
