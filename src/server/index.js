548c90fe16e2d97231653f8182f90c50f1b8785e
Joaquin Nicolas <joako.n96@gmail.com>
2018-11-29 21:02:44 -0300

    Add express body parser

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,7 @@ import {
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
 app.use('/api', userRouter);
 app.get('/', (req, res) => {
   res.send('Hello World');
